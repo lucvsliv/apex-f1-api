aaba2144cdbce7702294914fcdc75811f92b35f5
gongjuheon <yh6543210@gmail.com>
2025-12-03 21:39:43 +0900

    init: [APEX-2] OpenF1 API Driver 응답 DTO 추가

--- src/main/kotlin/com/lucvs/apex_f1_api/infrastructure/client/openf1/dto/OpenF1DriverResponse.kt
+++ src/main/kotlin/com/lucvs/apex_f1_api/infrastructure/client/openf1/dto/OpenF1DriverResponse.kt
@@ -1,3 +1,42 @@
 package com.lucvs.apex_f1_api.infrastructure.client.openf1.dto
 
-data class OpenF1DriverResponse()
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class OpenF1DriverResponse(
+
+    @JsonProperty("meeting_key")
+    val meetingKey: Int,
+
+    @JsonProperty("session_key")
+    val sessionKey: Int,
+
+    @JsonProperty("driver_number")
+    val driverNumber: Int,
+
+    @JsonProperty("broadcast_name")
+    val broadcastName: Int,
+
+    @JsonProperty("full_name")
+    val fullName: Int,
+
+    @JsonProperty("name_acronym")
+    val nameAcronym: Int,
+
+    @JsonProperty("team_name")
+    val teamName: Int,
+
+    @JsonProperty("team_colour")
+    val teamColour: Int,
+
+    @JsonProperty("first_name")
+    val firstName: Int,
+
+    @JsonProperty("last_name")
+    val lastName: Int,
+
+    @JsonProperty("headshot_url")
+    val headshotUrl: Int,
+
+    @JsonProperty("country_code")
+    val countryCode: Int
+)
