1a1669cbc08fb8ff44f9ca266a875615e565281c
gongjuheon <yh6543210@gmail.com>
2026-01-04 23:59:24 +0900

    chore: [APEX-13] ETL 공통 인터페이스 정의

--- src/main/kotlin/com/lucvs/apex_f1_api/application/service/etl/EtlProcessor.kt
+++ src/main/kotlin/com/lucvs/apex_f1_api/application/service/etl/EtlProcessor.kt
@@ -1,13 +1,4 @@
 package com.lucvs.apex_f1_api.application.service.etl
 
-import org.springframework.core.Ordered
-
-/**
- * 모든 도메인별 ETL 서비스가 구현해야 하는 인터페이스
- * Ordered 인터페이스를 상속받아 실행 순서를 제어합니다.
- * (예: Meeting 데이터가 있어야 Session을 연결할 수 있으므로 순서 중요)
- */
-interface EtlProcessor : Ordered {
-    fun process()
-    fun getName(): String // 로깅용 이름
+class EtlProcessor {
 }
