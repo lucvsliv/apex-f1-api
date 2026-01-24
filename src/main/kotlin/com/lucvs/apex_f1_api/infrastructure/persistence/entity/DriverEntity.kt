c0d47ca6b36d26a1e1489f55b736fd88a7c44e63
gongjuheon <yh6543210@gmail.com>
2026-01-24 23:56:31 +0900

    refact: Naming 불일치 해결을 위한 프로퍼티명 수정

--- src/main/kotlin/com/lucvs/apex_f1_api/infrastructure/persistence/entity/DriverEntity.kt
+++ src/main/kotlin/com/lucvs/apex_f1_api/infrastructure/persistence/entity/DriverEntity.kt
@@ -1,5 +1,6 @@
 package com.lucvs.apex_f1_api.infrastructure.persistence.entity
 
+import com.lucvs.apex_f1_api.domain.model.Driver
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
@@ -13,36 +14,36 @@ class DriverEntity(
 
     @Id
     @Column(name = "driver_number")
-    val driverNumber: Int,
+    val number: Int,
 
     @Column(name = "full_name")
-    val fullName: String,
+    val name: String,
 
     @Column(name = "country_code")
-    val countryCode: String,
+    val country: String,
 
     @Column(name = "team_name")
-    val teamName: String,
+    val team: String,
 
     @Column(name = "name_acronym")
-    val nameAcronym: String,
+    val acronym: String,
 
     @Column(columnDefinition = "TEXT")
     val description: String,
 
     @JdbcTypeCode(SqlTypes.OTHER)
     @Column(columnDefinition = "vector(1536")
-    var embedding: List<Double>? = null
+    var embedding: List<Double>? = null,
 ) {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true             // comparing memory address
         if (other !is DriverEntity) return false    // comparing class type
 
-        return driverNumber == other.driverNumber
+        return number == other.number
     }
 
     override fun hashCode(): Int {
-        return driverNumber.hashCode()
+        return number.hashCode()
     }
 }
