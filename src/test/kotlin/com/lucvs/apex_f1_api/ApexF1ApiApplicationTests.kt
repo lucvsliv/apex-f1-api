2203079930a1abb1b76875af6ec6ebc325392974
gongjuheon <yh6543210@gmail.com>
2026-03-21 11:25:29 +0900

    test: [APEX-57] CI 환경 구성을 위한 핵심 비즈니스 통합 테스트 최신화 및 실행 환경 격리

--- src/test/kotlin/com/lucvs/apex_f1_api/ApexF1ApiApplicationTests.kt
+++ src/test/kotlin/com/lucvs/apex_f1_api/ApexF1ApiApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.lucvs.apex_f1_api
 
+import io.github.bucket4j.distributed.proxy.ProxyManager
+import io.lettuce.core.RedisClient
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.bean.override.mockito.MockitoBean
 
 @SpringBootTest
+@ActiveProfiles("test") // 💡 1. application-test.yml을 읽도록 강제합니다.
 class ApexF1ApiApplicationTests {
+
+	// 💡 2. 스프링이 켜질 때 진짜 Redis를 찾지 못하도록 Mock 객체를 쥐여줍니다.
+	@MockitoBean
+	private lateinit var redisClient: RedisClient
+
+	@MockitoBean
+	private lateinit var bucket4jProxyManager: ProxyManager<ByteArray>
 
 	@Test
 	fun contextLoads() {
+		// 이 메서드가 에러 없이 통과하면 스프링 컨텍스트가 정상적으로 로드된 것입니다!
 	}
-
 }
